63db2bf61fd76d4f8a4cde1f839eb5ad243a9883
root <root@LAPTOP-EEV6NRCI.localdomain>
2022-11-17 15:54:04 -0600

    done

--- model.py
+++ model.py
@@ -4,16 +4,35 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
 
+
 class Cupcake(db.Model):
-    """This is our cupcake"""
-    
+    """Cupcake."""
+
     __tablename__ = "cupcakes"
-    
-    id = db.column(db.Integer , primary_key = True, autoincrement=True)
-    flavor = db.column(db.Text , nullable = False)
-    size = db.column(db.text , nullable = False)
-    rating = db.column(db.float , nullable = False)
-    image = db.column(db.text , nullable = False , default = DEFAULT_IMAGE)
-    
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
+
+    def to_dict(self):
+        """Serialize cupcake to a dict of cupcake info."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "rating": self.rating,
+            "size": self.size,
+            "image": self.image,
+        }
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
